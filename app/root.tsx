0f947ae71e9b6e2e41ac9d17b2657a30279aee1a
Eric Alas <alas.eric@gmail.com>
2022-06-01 11:44:43 -0400

    bring back map

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Storybites",
   viewport: "width=device-width,initial-scale=1",
 });
 
