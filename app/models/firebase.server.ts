904aac5b35dcb7a9d99a5ac28ea24adb610e1e48
Eric Alas <alas.eric@gmail.com>
2022-06-09 08:04:03 -0400

    use object keys not json in ev

--- app/models/firebase.server.ts
+++ app/models/firebase.server.ts
@@ -3,32 +3,9 @@ import type { UploadHandler } from "@remix-run/node";
 import { initializeApp } from "firebase/app";
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
 import { v4 as uuidv4 } from "uuid";
-let FIREBASE_CONFIG: any = {};
-let SERVICE_ACCOUNT: any = {};
-
-try {
-    FIREBASE_CONFIG = JSON.parse(process.env.FIREBASE_CONFIG?.toString() ?? "{}");
-} catch (e) {
-    try {
-        FIREBASE_CONFIG = JSON.parse(process.env.$FIREBASE_CONFIG?.toString() ?? "{}");
-    } catch (e) {
-        throw e;
-    }
-}
-
-try {
-    SERVICE_ACCOUNT = JSON.parse(process.env.SERVICE_ACCOUNT?.toString() ?? "{}");
-} catch (e) {
-    try {
-        SERVICE_ACCOUNT = JSON.parse(process.env.$SERVICE_ACCOUNT?.toString() ?? "{}");
-    } catch (e) {
-        throw e;
-    }
-}
 
 function getStorages() {
-    console.log(process.env.FIREBASE_CONFIG?.toString());
-    FIREBASE_CONFIG = JSON.parse(process.env.FIREBASE_CONFIG?.toString() ?? "{}");
+    const { FIREBASE_CONFIG, SERVICE_ACCOUNT } = getCreds();
 
     const app = initializeApp(FIREBASE_CONFIG);
     const readStorage = getStorage(app);
@@ -44,6 +21,8 @@ function getStorages() {
 }
 
 export async function uploadFileV2(data: Buffer, fileName: string, folder: string) {
+    const { FIREBASE_CONFIG } = getCreds();
+
     try {
         const { uploadStorage } = getStorages();
         const file = uploadStorage.bucket(FIREBASE_CONFIG.storageBucket).file(`${folder}/${fileName}`);
@@ -89,4 +68,36 @@ export async function getAudioUrl(name: string) {
     const imageRef = ref(readStorage, `audio/${name}`);
     const url = await getDownloadURL(imageRef);
     return url;
+}
+
+function getCreds() {
+    const projectId = process.env.PROJECT_ID;
+    const FIREBASE_CONFIG = {
+        apiKey: process.env.API_KEY,
+        authDomain: `${projectId}.firebaseapp.com`,
+        projectId,
+        storageBucket: `${projectId}.appspot.com`,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
+    };
+
+    const SERVICE_ACCOUNT = {
+        type: "service_account",
+        project_id: projectId,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url:
+            "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rzh6t%40storybites-qa.iam.gserviceaccount.com",
+    };
+
+    return {
+        FIREBASE_CONFIG,
+        SERVICE_ACCOUNT,
+    };
 }
